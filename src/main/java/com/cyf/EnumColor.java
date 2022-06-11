54bbc2ec68b502de15113eab142292fb7a25d9cd
minicao1997 <54624553+minicao1997@users.noreply.github.com>
2022-06-11 15:58:41 +0800

    我在GitHub上添加一点信息

--- src/main/java/com/cyf/EnumColor.java
+++ src/main/java/com/cyf/EnumColor.java
@@ -10,6 +10,6 @@ public class EnumColor {
         System.out.println("我在hot-fix分支中添加了一条消息");
         System.out.println("在master分支中添加了一条消息");
         System.out.println("我再添加一点信息");
-
+        System.out.println("我在GitHub上添加一点信息");
     }
 }
